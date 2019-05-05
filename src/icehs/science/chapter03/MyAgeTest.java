34d802f4c3b0f62be65ba7f4f4a5a2b1b89ed8b7
구병국 <@DESKTOP-VIRS052>
2019-05-05 17:15:09 +0900

    Chapter03

---
+++ src/icehs/science/chapter03/MyAgeTest.java
@@ -0,0 +1,17 @@
+package icehs.science.chapter03;
+
+public class MyAgeTest {
+
+	public static void main(String[] args) {
+		int bornYear = 1969;
+		int myAge = 50;
+		int myDay;
+		
+		System.out.println(bornYear);
+		/*
+		 * System.out.println(myAge); 
+		 * System.out.println(myDay);
+		 */
+	}
+
+}
